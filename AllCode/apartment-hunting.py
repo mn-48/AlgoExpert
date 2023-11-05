32c3d7128e4d977206b51d5e131b7b17c6dabc39
nazmul-cse48 <nazmul.cse48@gmail.com>
2023-11-05 22:15:13 +0600

    line through points

--- AllCode/apartment-hunting.py
+++ AllCode/apartment-hunting.py
@@ -36,7 +36,7 @@ blocks = [
     {"gym": False, "school": True, "store": False},
     {"gym": True, "school": False, "store": False},
     {"gym": True, "school": True, "store": False},
-    {"gym": False, "school": True, "store": False},
+    {"gym": False, "school": True, "store": False}, 
     {"gym": False, "school": True, "store": True},
 ]
 reqs = ["gym", "school", "store"]
---
+++ Current/A.py
@@ -0,0 +1,37 @@
+from fractions import Fraction
+
+def lineThroughPoints(points):
+    max_lines_through_points = 1
+    for i in range(len(points)-1):
+        x1, y1 = points[i]
+        slopes = {}
+
+        for j in range(i+1, len(points)):
+            x2, y2 = points[j]
+
+            slope = Fraction(y2-y1, x2-x1) if x1 != x2 else float("inf")
+
+            numerator, denominator = (1, 0) if slope == float("inf") else (slope.numerator, slope.denominator)
+
+            if (numerator, denominator) not in slopes:
+                slopes[(numerator, denominator)] = 1
+            slopes[(numerator, denominator)] += 1
+        max_lines_through_current_pivot = max(slopes.values())
+        max_lines_through_points = max(max_lines_through_points, max_lines_through_current_pivot)
+
+    return max_lines_through_points
+
+
+ 
+
+points =[
+  [1, 1],
+  [2, 2],
+  [3, 3],
+  [0, 4],
+  [-2, 6],
+  [4, 0],
+  [2, 1]
+]
+
+print(lineThroughPoints(points))
