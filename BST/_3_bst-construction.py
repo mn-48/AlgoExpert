8e7d405fc41057aceff18f4a41e2e81783605e2a
nazmul-cse48 <nazmul.cse48@gmail.com>
2023-11-13 23:58:29 +0600

    BST contraction

--- BST/_3_bst-construction.py
+++ BST/_3_bst-construction.py
@@ -8,26 +8,31 @@ class BST:
         self.left = None
         self.right = None
 
+    # Average o(log(n)) Time | O(1) Space
+    # Wrost o(n) Time | O(1) Space
     def insert(self, value):
         currentNode = self
-        while currentNode is not None:
+        while True:
             if value < currentNode.value:
-                if currentNode.left:
-                    currentNode = currentNode.left
-                else:
+                if currentNode.left is None:
                     currentNode.left = BST(value)
                     break
-            elif value >= currentNode.value:
-                if currentNode.right:
-                    currentNode = currentNode.right
                 else:
-                    currentNode.right = BST(value) 
+                    currentNode = currentNode.left
+                    
+            else:
+                if currentNode.right is None:
+                    currentNode.right = BST(value)
                     break
+                else:
+                    currentNode = currentNode.right
         return self
 
+    # Average o(log(n)) Time | O(1) Space
+    # Wrost o(n) Time | O(1) Space
     def contains(self, value):
-        # Write your code here.
         currentNode = self
+
         while currentNode is not None:
             if currentNode.value == value:
                 return True
@@ -35,9 +40,11 @@ class BST:
                 currentNode = currentNode.left
             else:
                 currentNode = currentNode.right
+            
         return False
 
-
+    # Average o(log(n)) Time | O(1) Space
+    # Wrost o(n) Time | O(1) Space
     def remove(self, value, parentNode=None):
         # Write your code here.
         # Do not edit the return statement of this method.
@@ -104,3 +111,40 @@ class BST:
             node = node.max 
         return node
 
+
+    # def remove(self, value, parentNode = None):
+    #     currentNode = self
+    #     while currentNode is not None:
+    #         if value < currentNode.value:
+    #             parentNode = currentNode
+    #             currentNode = currentNode.left
+    #         elif value > currentNode.value:
+    #             parentNode = currentNode
+    #             currentNode = currentNode.right
+    #         else:
+    #             if currentNode.left is not None and currentNode.right is not None:
+    #                 currentNode.value = currentNode.right.getMinValue()
+    #                 currentNode.right.remove(currentNode.value, currentNode)
+    #             elif parentNode is None:
+    #                 if currentNode.left is not None:
+    #                     currentNode.value = currentNode.left.value
+    #                     currentNode.right = currentNode.left.right
+    #                     currentNode.left = currentNode.left.left
+    #                 elif currentNode.right is not None:
+    #                     currentNode.value = currentNode.right.value
+    #                     currentNode.left = currentNode.right.left
+    #                     currentNode.right = currentNode.right.right
+    #                 else:
+    #                     currentNode.value = None
+    #             elif parentNode.left == currentNode:
+    #                 parentNode.left = currentNode.left if currentNode.left is not None else currentNode.right
+    #             elif parentNode.right==currentNode:
+    #                 parentNode.right = currentNode.left if currentNode.left is not None else currentNode.right
+    #             break
+    #     return self
+    
+    # def getMinValue(self):
+    #     currentNode = self
+    #     while currentNode.left is not None:
+    #         currentNode = currentNode.left
+    #     return currentNode.value
---
+++ Current/test.py
@@ -0,0 +1,23 @@
+# This file is initialized with a code version of this
+# question's sample test case. Feel free to add, edit,
+# or remove test cases in this file as you see fit!
+
+import program
+import unittest
+
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        array = [
+            [0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0, 0],
+        ]
+        source = 3
+        expected = [0, 0, 0, 25, 25, 0, 0]
+        actual = program.waterfallStreams(array, source)
+        self.assertEqual(actual, expected)
