114b884539b746282705778431907977f59e0fc4
mn-48 <nazmul.cse48@gmail.com>
2024-02-10 12:43:21 +0600

    iterativeInOrderTraversal

--- BT/BT_13_iterative-in-order-traversal.py
+++ BT/BT_13_iterative-in-order-traversal.py
@@ -1,13 +1,37 @@
+# # O(n) Time | O(1) Space
+
+# def iterativeInOrderTraversal(tree, callback):
+#     # Write your code here
+#     stack = []
+#     while len(stack) or tree is not None:
+#         while tree is not None:
+#             stack.append(tree)
+#             tree = tree.left
+#         tree = stack.pop()
+#         callback(tree)
+#         tree = tree.right
+#     return
+
+
 # O(n) Time | O(1) Space
 
 def iterativeInOrderTraversal(tree, callback):
-    # Write your code here
-    stack = []
-    while len(stack) or tree is not None:
-        while tree is not None:
-            stack.append(tree)
-            tree = tree.left
-        tree = stack.pop()
-        callback(tree)
-        tree = tree.right
-    return
+    previousNode = None
+    currentNode = tree
+
+    while currentNode is not None:
+        if previousNode is None or previousNode==currentNode.parent:
+            if currentNode.left is not None:
+                nextNode = currentNode.left
+            else:
+                callback(currentNode)
+                nextNode = currentNode.right if currentNode.right is not None else currentNode.parent
+               
+        elif previousNode == currentNode.left:
+            callback(currentNode)
+            nextNode = currentNode.right if currentNode.right is not None else currentNode.parent
+        else:
+            nextNode = currentNode.parent
+        previousNode = currentNode
+        currentNode = nextNode
+
