3be6281167a1ef10d619004af47d74a8d68831d0
mn-48 <nazmul.cse48@gmail.com>
2024-02-17 01:37:47 +0600

    Alhamdulillah. binary tree done

--- BT/BT_15_all-kinds-of-node-depths.py
+++ BT/BT_15_all-kinds-of-node-depths.py
@@ -5,15 +5,84 @@ class BinaryTree:
         self.value = value
         self.left = None
         self.right = None
+# # Iterative Solution ==========================
+# # O(nlog(n)) Time | O(h) Space 
+# def allKindsOfNodeDepths(root):
+#     sumOfAllDepths = 0
+#     stack = [root]
 
-# O(nlog(n)) Time | O(h) Space 
+#     while len(stack) > 0:
+#         node = stack.pop()
+#         if node is None:
+#             continue
+#         sumOfAllDepths += nodeDepths(node)
+#         stack.append(node.left)
+#         stack.append(node.right)
+#     return sumOfAllDepths
+
+
+# Recursive Solution ======================= 
+# O(nlog(n)) Time | O(h) Space  
+
+# def allKindsOfNodeDepths(root):
+#     if root is None:
+#         return 0
+#     # Write your code here.
+#     return allKindsOfNodeDepths(root.left)+allKindsOfNodeDepths(root.right)+nodeDepths(root)
+
+# def nodeDepths(node, depth=0):
+#     if node is None:
+#         return 0
+#     return depth + nodeDepths(node.left, depth+1) + nodeDepths(node.right, depth+1)
+
+# #  solve with touple ===========================
+# # O(n) Time | O(d) Space
+# def allKindsOfNodeDepths(root):
+#     sum_all = 0
+#     stack = []
+#     current, depth, depth_sum = root, 0, 0
+
+#     while True:
+#         if current:
+#             depth_sum += depth
+#             sum_all += depth_sum
+#             stack.append((current, depth, depth_sum))
+#             current, depth = current.left, depth+1
+#         elif stack:
+#             popped, depth, depth_sum = stack.pop()
+#             current, depth = popped.right, depth+1
+#         else:
+#             break
+            
+#     return sum_all
+
+
+#  official solve  ===========================
+# O(n) Time | O(d) Space
+
 def allKindsOfNodeDepths(root):
-    if root is None:
-        return 0
-    # Write your code here.
-    return allKindsOfNodeDepths(root.left)+allKindsOfNodeDepths(root.right)+nodeDepths(root)
+    return getTreeInfo(root).sumOfAllDepths
 
-def nodeDepths(node, depth=0):
-    if node is None:
-        return 0
-    return depth + nodeDepths(node.left, depth+1) + nodeDepths(node.right, depth+1)
+def getTreeInfo(tree):
+    if tree is None:
+        return TreeInfo(0, 0, 0)
+    
+    leftTreeInfo = getTreeInfo(tree.left)
+    rightTreeInfo = getTreeInfo(tree.right)
+
+    sumOfLeftDepths = leftTreeInfo.sumOfDepths + leftTreeInfo.numNodesInTree
+    sumOfRightDepths = rightTreeInfo.sumOfDepths + rightTreeInfo.numNodesInTree
+
+    numNodesInTree = 1 + leftTreeInfo.numNodesInTree+ rightTreeInfo.numNodesInTree
+    sumOfDepths = sumOfLeftDepths + sumOfRightDepths
+    sumOfAllDepths = sumOfDepths+leftTreeInfo.sumOfAllDepths +rightTreeInfo.sumOfAllDepths
+
+    return TreeInfo(numNodesInTree, sumOfDepths, sumOfAllDepths)
+    
+
+
+class TreeInfo:
+    def __init__(self, numNodesInTree, sumOfDepths, sumOfAllDepths):
+        self.numNodesInTree = numNodesInTree
+        self.sumOfDepths = sumOfDepths
+        self.sumOfAllDepths = sumOfAllDepths
---
+++ BT/BT_16_compare-leaf-traversal.py
@@ -0,0 +1,30 @@
+# This is an input class. Do not edit.
+class BinaryTree:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+# O(m+n) time | o(max(h1, h2)) Space
+def compareLeafTraversal(tree1, tree2):
+    array1 = []
+    array2 = []
+    arrayOfLeaves(tree1, array1)
+    arrayOfLeaves(tree2, array2)
+    # Write your code here.
+    if array1==array2:
+        return True
+    else:
+        return False
+
+
+
+def arrayOfLeaves(tree, array):
+    if tree is None:
+        return
+    arrayOfLeaves(tree.left, array)
+    if tree.left is None and tree.right is None:
+        array.append(tree.value)
+    arrayOfLeaves(tree.right, array)
+
