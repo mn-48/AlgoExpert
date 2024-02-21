753f2a794caa4c109dc7b06626713e3f9399995e
mn-48 <nazmul.cse48@gmail.com>
2024-02-21 20:31:49 +0600

    numberOfWaysToTraverseGraph

--- DP/DP_5_number-of-ways-to-traverse-graph.py
+++ DP/DP_5_number-of-ways-to-traverse-graph.py
@@ -7,6 +7,13 @@
 #     return numberOfWaysToTraverseGraph(width-1, height)+numberOfWaysToTraverseGraph(width, height-1)
 
 # ==========================================================
+
+#  _______________________________
+# |___0__|___1__|__1____|__1______|
+# |___1__|___2__|__3____|__4______|
+# |___1__|___3__|__6____|__10_____|
+
+
 # O(m*n) Time | O(m*n) Space
 def numberOfWaysToTraverseGraph(width, height):
     numberOfWays = [[0]*(width+1) for y in range(height+1)]
@@ -21,9 +28,22 @@ def numberOfWaysToTraverseGraph(width, height):
     return numberOfWays[width][height]
 
 # ==================================================================
-# |___0__|___1__|__1____|__1______|
-# |___1__|___2__|__3____|__4______|
-# |___1__|___3__|__6____|__10_____|
+
+
+# # O(m+n) Time | O(1) Space
+# '''width = 4, height =3
+#  _______________________________
+# |Start_|___R__|__R____|__R______|
+# |___D__|______|_______|_________|
+# |___D__|______|_______|__End____|
+
+# '''
+# import math 
+# def numberOfWaysToTraverseGraph(width, height):
+#     # Write your code here.
+#     # (R+D)!/(R!*D|!)
+#     return math.comb(width+height-2, width-1)
+
 # O(m+n) Time | O(1) Space
 def numberOfWaysToTraverseGraph(width, height):
     xDistanceToCorner = width-1
@@ -40,20 +60,6 @@ def factorial(n):
         res*=i
     return res
 
-
-# # O(m+n) Time | O(1) Space
-# '''width = 4, height =3
-#  _______________________________
-# |Start_|___R__|__R____|__R______|
-# |___D__|______|_______|_________|
-# |___D__|______|_______|__End____|
-
-# '''
-# import math 
-# def numberOfWaysToTraverseGraph(width, height):
-#     # Write your code here.
-#     # (R+D)!/(R!*D|!)
-#     return math.comb(width+height-2, width-1)
 
 if __name__=="__main__":
     width = 4
