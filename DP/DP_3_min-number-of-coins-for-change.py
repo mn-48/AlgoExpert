9777863f52b7af1bfd24cc5fdb2028311eabbd17
mn-48 <nazmul.cse48@gmail.com>
2024-02-21 10:47:35 +0600

    levenshteinDistance

--- DP/DP_3_min-number-of-coins-for-change.py
+++ DP/DP_3_min-number-of-coins-for-change.py
@@ -5,7 +5,7 @@ def minNumberOfCoinsForChange(n, denoms):
     numberOfCoins = [float('inf')]*(n+1)
     numberOfCoins[0] = 0
     for denom in denoms:
-        for amount in range(1, n):
+        for amount in range(len(numberOfCoins)):
             if denom <= amount: 
                 numberOfCoins[amount] = min(numberOfCoins[amount], 1+numberOfCoins[amount-denom])
     return numberOfCoins[n] if numberOfCoins[n] != float('inf') else -1
