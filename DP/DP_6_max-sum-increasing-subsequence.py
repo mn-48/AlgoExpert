ff4aa66998d8f04beb689d7e14f335255bd67804
mn-48 <nazmul.cse48@gmail.com>
2024-02-24 22:55:11 +0600

    maxSumIncreasingSubsequence

---
+++ DP/DP_6_max-sum-increasing-subsequence.py
@@ -0,0 +1,27 @@
+def maxSumIncreasingSubsequence(array):
+    # sequences  = [None] * len(array)
+    sequences  = [None for x in array] 
+    sums = array[:]
+    maxSumIdx = 0
+    for i in range(len(array)):
+        currentNumber = array[i]
+        
+        for j in range(0, i):
+            otherNumber = array[j]
+            
+            if otherNumber < currentNumber and sums[j] + currentNumber >= sums[i]:
+                sums[i] = sums[j] + currentNumber
+                sequence[i] = j
+        if sums[i] >= sums[maxSumIdx]:
+            maxSumIdx = i
+    return [sums[maxSumIdx], buildSequence(array, sequences, maxSumIdx)]
+
+
+def  buildSequence(array, sequences, currentIdx):
+    sequence = []
+    while currentIdx is not None:
+        sequence.append(array[currentIdx])
+        currentIdx = sequences[currentIdx]
+    return list(reversed(sequence))           
+            
+    
