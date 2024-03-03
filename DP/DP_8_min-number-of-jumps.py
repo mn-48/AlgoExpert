28f59fd1bfabc210832d54f42e5704ccca028083
mn-48 <nazmul.cse48@gmail.com>
2024-03-03 06:32:16 +0600

    range(1, len(array))

--- DP/DP_8_min-number-of-jumps.py
+++ DP/DP_8_min-number-of-jumps.py
@@ -2,7 +2,7 @@
 def minNumberOfJumps(array):
     jumps = [float('inf') for i in array]
     jumps[0] = 0
-    for i in range(len(array)):
+    for i in range(1, len(array)):
         for j in range(0, i):
             if array[j] + j>=i:
                 jumps[i] = min(jumps[j]+1, jumps[i])
