a73ce6ee9bf9aec8eb774c6a9109801300d2c275
mn-48 <nazmul.cse48@gmail.com>
2024-07-03 06:41:43 +0600

    revealMinesweeper

--- Recursion/Recursion_8_reveal-minesweeper.py
+++ Recursion/Recursion_8_reveal-minesweeper.py
@@ -1,12 +1,46 @@
 # O(w*h) Time | O(w*h) Space
 
 def revealMinesweeper(board, row, column):
-    # Write your code here.
-    return []
-
-
-if __name__=="__main__":
+    if board[row][column] == "M":
+        board[row][column] = "X"
+        return board
+    neighbors = getNeighbors(board, row, column)
     
+    adjacentMinesCount = 0
+    for neighborRow,  neighborColumn in neighbors: # O(8) this does not affect the solutions
+        if board[neighborRow][neighborColumn] == "M":
+            adjacentMinesCount += 1
+    if adjacentMinesCount > 0:
+        board[row][column] = str(adjacentMinesCount)
+    else:
+        board[row][column] = '0'
+        for neighborRow,  neighborColumn in neighbors:
+            if board[neighborRow][neighborColumn]=="H":
+                revealMinesweeper(board,  neighborRow,  neighborColumn)
+    
+
+    return board
+
+
+def getNeighbors(board, row, column):
+    directions = [
+        [-1, 1], [0, 1], [1, 1],
+        [-1, 0], [1, 0],
+        [-1, -1], [0, -1], [1, -1]
+    ]
+
+    neighbors = []
+    for directionRow, directionColumn in directions:
+        new_row = row + directionRow
+        new_column = column + directionColumn
+        
+        if 0<= new_row < len(board) and 0<= new_column < len(board[0]):
+            neighbors.append([new_row, new_column])
+    return neighbors
+
+
+if __name__ == "__main__":
+
     board = [
         ["H", "H", "H", "H", "M"],
         ["H", "1", "M", "H", "1"],
@@ -21,6 +55,6 @@ if __name__=="__main__":
         ["0", "1", "1", "1", "0"],
         ["0", "0", "0", "0", "0"],
     ]
-    
+
     ans = revealMinesweeper(board, row, column)
     print(ans)
