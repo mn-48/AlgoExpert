4ee6b2857dc8edd515ab10f3d4b9988866e55416
mn-48 <nazmul.cse48@gmail.com>
2024-07-26 12:32:43 +0600

    valid subsequence

--- AllCode/Array/Array_2_validate-subsequence_1.py
+++ AllCode/Array/Array_2_validate-subsequence_1.py
@@ -1,3 +1,16 @@
+# # Time O(n) | Space O(1)
+
+# def isValidSubsequence(array, sequence):
+#     seq_indx = 0
+#     for a in array:
+#         if seq_indx==len(sequence):
+#             return True
+#         if sequence[seq_indx] == a:
+#             seq_indx += 1
+#     return seq_indx == len(sequence)
+
+
+
 # Time O(n) | Space O(1)
 def isValidSubsequence(array, sequence):
     # Write your code here.  
