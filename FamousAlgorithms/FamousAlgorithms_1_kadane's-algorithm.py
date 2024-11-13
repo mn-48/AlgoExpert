4a080a26dee723002c61c50158a6082a5650a71f
mn-48 <nazmul.cse48@gmail.com>
2024-11-14 00:23:01 +0600

    kadanes algorithm dp problem

--- FamousAlgorithms/FamousAlgorithms_1_kadane's-algorithm.py
+++ FamousAlgorithms/FamousAlgorithms_1_kadane's-algorithm.py
@@ -7,5 +7,6 @@ def kadanesAlgorithm(array):
         max_sum, current_sum = max(max_sum, current_sum), max(0, current_sum)
     return max_sum
 if __name__=="__main__":
-    ans = kadanesAlgorithm([3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4])
+    # ans = kadanesAlgorithm([3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4])
+    ans = kadanesAlgorithm([-1, -2, -3, -5])
     print(ans)
