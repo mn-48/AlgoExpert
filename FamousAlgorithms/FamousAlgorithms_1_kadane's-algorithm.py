3db6ae28fef9d9d91bccf169eb6471a2a142b8de
mn-48 <nazmul.cse48@gmail.com>
2024-11-13 23:25:29 +0600

    kadanesAlgorithm

---
+++ FamousAlgorithms/FamousAlgorithms_1_kadane's-algorithm.py
@@ -0,0 +1,11 @@
+# O(n) Time | O(1) 
+def kadanesAlgorithm(array):
+    # Write your code here.
+    max_sum, current_sum = float('-inf'), 0
+    for num in array:
+        current_sum += num
+        max_sum, current_sum = max(max_sum, current_sum), max(0, current_sum)
+    return max_sum
+if __name__=="__main__":
+    ans = kadanesAlgorithm([3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4])
+    print(ans)
