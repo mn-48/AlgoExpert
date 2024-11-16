346bb9049ff1081bcde80023c9bec97714c84242
mn-48 <nazmul.cse48@gmail.com>
2024-11-16 21:46:34 +0600

    Union

--- FamousAlgorithms/FamousAlgorithms_1_kadane's-algorithm.py
+++ FamousAlgorithms/FamousAlgorithms_1_kadane's-algorithm.py
@@ -6,6 +6,10 @@ def kadanesAlgorithm(array):
         current_sum += num
         max_sum, current_sum = max(max_sum, current_sum), max(0, current_sum)
     return max_sum
+
+
+
+
 if __name__=="__main__":
     # ans = kadanesAlgorithm([3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4])
     ans = kadanesAlgorithm([-1, -2, -3, -5])
--- FamousAlgorithms/FamousAlgorithms_3_union-find.py
+++ FamousAlgorithms/FamousAlgorithms_3_union-find.py
@@ -3,26 +3,61 @@
 # and union methods. Feel free to add new
 # properties and methods to the class.
 
-# O(1) Time | O(1) space
+# # O(1) Time | O(1) space
+# class UnionFind:
+#     def __init__(self):
+#         self.parents = {}
+
+#     def createSet(self, value):
+#         self.parents[value] = value
+
+#     def find(self, value):
+#         if value not in self.parents:
+#             return None
+#         while value != self.parents[value]:
+#             value = self.parents[value]
+#         return value
+
+
+#     def union(self, valueOne, valueTwo):
+#         if valueOne not in self. parents or valueTwo not in self.parents:
+#             return
+#         valueOneRoot = self.find(valueOne)
+#         valueTwoRoot = self.find(valueTwo)
+#         self.parents[valueTwoRoot]=valueOneRoot
+
 class UnionFind:
     def __init__(self):
         self.parents = {}
+        self.ranks = {}
 
+    # O(1) Time| O(1) Space
     def createSet(self, value):
         self.parents[value] = value
+        self.ranks[value] = 0
+
+    # O(α(n)) aproximately O(1) Time| O(1) Space
 
     def find(self, value):
         if value not in self.parents:
             return None
-        while value != self.parents[value]:
-            value = self.parents[value]
-        return value
+        if value != self.parents[value]:
+            self.parents[value] = self.find(self.parents[value])
+        return self.parents[value]
 
-    
+    # O(α(n)) aproximately O(1) Time | O(1) Space
 
     def union(self, valueOne, valueTwo):
         if valueOne not in self. parents or valueTwo not in self.parents:
             return
+
         valueOneRoot = self.find(valueOne)
         valueTwoRoot = self.find(valueTwo)
-        self.parents[valueTwoRoot]=valueOneRoot
+
+        if self.ranks[valueOneRoot] < self.ranks[valueTwoRoot]:
+            self.parents[valueOneRoot] = valueTwoRoot
+        elif self.ranks[valueOneRoot] > self.ranks[valueTwoRoot]:
+            self.parents[valueTwoRoot] = valueOneRoot
+        else:
+            self.parents[valueTwoRoot] = valueOneRoot
+            self.ranks[valueOneRoot] += 1
