f219c09b2765ec2b06f57944a44933737ece2dcd
mn-48 <nazmul.cse48@gmail.com>
2024-12-14 22:28:50 +0600

    laptopRentals

--- Heaps/Heaps_4_laptop-rentals.py
+++ Heaps/Heaps_4_laptop-rentals.py
@@ -1,18 +1,40 @@
 #  O(nlog(n)) Time | O(n) Space
  
-import heapq
+# import heapq
 
+# def laptopRentals(times):
+#     times.sort(key=lambda x:x[0])
+#     # print(times)
+#     rented = []
+
+#     for start, end in times:
+#         if rented and rented[0] <= start:
+#             heapq.heappop(rented)
+#         heapq.heappush(rented, end)
+
+#     return len(rented)
+
+
+
+# O(nlog(n)) Time | O(n) Space
 def laptopRentals(times):
-    times.sort(key=lambda x:x[0])
-    # print(times)
-    rented = []
+    if len(times) == 0:
+        return 0
+    usedLaptops = 0
+    startTimes = sorted([t[0] for t in times])
+    endTimes = sorted([t[1] for t in times])
 
-    for start, end in times:
-        if rented and rented[0] <= start:
-            heapq.heappop(rented)
-        heapq.heappush(rented, end)
+    i = 0
+    j = 0 
+    while i < len(times):
+        if startTimes[i] >= endTimes[j]:
+            usedLaptops -= 1
+            j+=1
+        usedLaptops += 1
+        i +=1
+    
+    return usedLaptops
 
-    return len(rented)
 
 
 if __name__=="__main__":
