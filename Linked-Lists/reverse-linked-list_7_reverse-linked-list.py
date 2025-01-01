6f3f9b84e593e7c43546f4d285a00eb25f5bc723
mn-48 <nazmul.cse48@gmail.com>
2025-01-01 23:50:37 +0600

    reverseLinkedList

--- Linked-Lists/reverse-linked-list_7_reverse-linked-list.py
+++ Linked-Lists/reverse-linked-list_7_reverse-linked-list.py
@@ -1,18 +1,36 @@
+# # This is an input class. Do not edit.
+# class LinkedList:
+#     def __init__(self, value):
+#         self.value = value
+#         self.next = None
+
+# # O(n) Time | O(1) Space
+# def reverseLinkedList(head):
+#     previousNode, currentNode = None, head
+
+#     while currentNode is not None:
+#         nextNode = currentNode.next
+#         currentNode.next = previousNode
+#         previousNode = currentNode
+#         currentNode = nextNode
+#     return previousNode
+        
+        
 # This is an input class. Do not edit.
 class LinkedList:
     def __init__(self, value):
         self.value = value
         self.next = None
 
+
 # O(n) Time | O(1) Space
 def reverseLinkedList(head):
-    previousNode, currentNode = None, head
+    p1, p2 = None, head
 
-    while currentNode is not None:
-        nextNode = currentNode.next
-        currentNode.next = previousNode
-        previousNode = currentNode
-        currentNode = nextNode
-    return previousNode
+    while p2 is not None:
+        p3 = p2.next
+        p2.next = p1
+        p1 = p2
+        p2 = p3
         
-        
+    return p1
