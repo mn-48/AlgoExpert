e0e29003caa119ac10164675fa512a4f1c138cc8
mn-48 <nazmul.cse48@gmail.com>
2025-01-04 15:59:04 +0600

    Lonked list

--- Linked-Lists/LinkedList_9_shift-linked-list.py
+++ Linked-Lists/LinkedList_9_shift-linked-list.py
@@ -5,33 +5,63 @@ class LinkedList:
         self.next = None
 
 
+# # O(n) time | O(1) space
+# def shiftLinkedList(head, k):
+#     p2=p3=p1= head
+#     totalNode = 0
+
+#     while head:
+#         totalNode +=1
+#         head = head.next
+        
+#     k = k% totalNode
+
+#     if k==0:
+#         return p1
+        
+#     s = 0
+#     while  s<k:
+#         p2 = p2.next
+#         s+=1
+        
+#     while p2.next:
+#         p2 = p2.next
+#         p3 = p3.next
+
+#     newHead = p3.next
+#     p2.next = p1
+#     p3.next = None
+
+#     return newHead
+        
+
+
+# This is the class of the input linked list.
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
 # O(n) time | O(1) space
 def shiftLinkedList(head, k):
-    p2=p3=p1= head
-    totalNode = 0
+    listLength = 1
+    listTail = head
 
-    while head:
-        totalNode +=1
-        head = head.next
-        
-    k = k% totalNode
+    while listTail.next is not None:
+        listTail = listTail.next
+        listLength += 1
 
-    if k==0:
-        return p1
-        
-    s = 0
-    while  s<k:
-        p2 = p2.next
-        s+=1
-        
-    while p2.next:
-        p2 = p2.next
-        p3 = p3.next
+    offset = abs(k) % listLength
+    if offset == 0:
+        return head
 
-    newHead = p3.next
-    p2.next = p1
-    p3.next = None
+    newTailPosition = listLength - offset if k > 0 else offset
+    newTail = head
+    for i in range(1, newTailPosition):
+        newTail = newTail.next
 
+    newHead = newTail.next
+    newTail.next = None
+    listTail.next = head
     return newHead
-        
-
