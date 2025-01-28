7760392dcb98bebf1e10c517377d7b9d6a92a4e1
mn-48 <nazmul.cse48@gmail.com>
2025-01-28 22:48:21 +0600

    searchForRange

---
+++ Searching/Searching_5_search-for-range.py
@@ -0,0 +1,35 @@
+# O(lon(n)) time | O(1) space
+
+def searchForRange(array, target):
+    finalRange = [-1, -1]
+    alteredBinarySearch(array, target, 0, len(array) - 1, finalRange, True)
+    alteredBinarySearch(array, target, 0, len(array) - 1, finalRange, False)
+    return finalRange
+
+
+def alteredBinarySearch(array, target, left, right, finalRange, goLeft):
+    while left <= right:
+        mid = (left + right) // 2
+        if array[mid] < target:
+            left = mid + 1
+        elif array[mid] > target:
+            right = mid - 1
+        else:
+            if goLeft:
+                if mid == 0 or array[mid - 1] != target:
+                    finalRange[0] = mid
+                    return
+                else:
+                    right = mid - 1
+            else:
+                if mid == len(array) - 1 or array[mid + 1] != target:
+                    finalRange[1] = mid
+                    return
+                else:
+                    left = mid + 1
+
+
+if __name__ == "__main__":
+    ans = searchForRange(
+        [0, 1, 21, 33, 45, 45, 45, 45, 45, 45, 61, 71, 73], 45)
+    print(ans)
