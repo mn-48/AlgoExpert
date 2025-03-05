d237a64805402358f926a0f7259e7b23fd4cbe28
Md. Nazmul Hossain <30841411+mn-48@users.noreply.github.com>
2025-03-05 19:41:28 +0600

    Create Stacks_2_balanced-brackets.py

---
+++ Stacks/Stacks_2_balanced-brackets.py
@@ -0,0 +1,20 @@
+# O(n) Time | O(n) Space
+
+def balancedBrackets(string):
+    openingBrackets = "({["
+    closingBrackets = ")}]"
+    matchingBrackets = {")":"(", "}":"{", "]":"["}
+
+    stack = []
+
+    for char in string:
+        if char in openingBrackets:
+            stack.append(char)
+        elif char in closingBrackets:
+            if len(stack)==0:
+                return False
+            if stack[-1]== matchingBrackets[char]:
+                stack.pop()
+            else:
+                return False
+    return len(stack) == 0
